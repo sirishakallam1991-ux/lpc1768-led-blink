4a0d248b19ea868119e5d5354ad07eb36c3b4f80
sirisha Kallam <sirishakallam1991@gmail.com>
2026-05-29 10:35:45 -0500

    fix: correct array declaration in Led_Pattern.c

--- Led_Pattren.c
+++ Led_Pattren.c
@@ -4,7 +4,8 @@ void delay(unsigned int x);
 
 int main(){
 
-int i,pattern={0X81,0X42,0X24,0X18,0X24,0X42,0X81};
+int i;
+unsigned int pattern[]={0X81,0X42,0X24,0X18,0X24,0X42,0X81};
 
 LPC_GPIO0->FIODIR  =(0xFF<<19);	//configure the p0.o as output port 
 
